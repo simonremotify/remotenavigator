1ac4a75b7084afaa2d08be2a97637d71a2ee83f3
Simon Deeley <simon@remotify.work>
2020-05-23 16:30:49 +0100

    Produce a focussed diagram for every node; Make it possible to choose subsets Tweaks and corrections to the model Record the command used to make diagrams for my FEN presentation

---
+++ fendiagrams.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+bin/focuson --subset "Career stagnation,Career progression model,Alignment" \
+	    --subset "Career stagnation,Career progression model,Alignment,Repeatable processes,Accountability" \
+	    --subset "Hampered collaboration,Effective Meetings,Realtime collaboration tools,Shared and organised information,Asynchronous working,Effective meetings,Repeatable processes,Outcome quality" \
+	    --subset "Loneliness,Planned spontaneity,Buddy system,Flexible working,Community involvement,Inclusion,Alignment,Diversity,Human connection,Employee satisfaction,Retention,Outcome quality" \
+	    --subset "Loneliness,Planned spontaneity,Buddy system,Community involvement,Inclusion,Human connection,Employee satisfaction,Retention" \
+	    --subset "Loneliness,Buddy system,Flexible working,Community involvement,Alignment,Diversity,Outcome quality" \
+	    --subset "Career stagnation,Isolation,Onboarding,Loneliness,Planned spontaneity" \
+	    --subset "Asynchronous working,Flexible working,Community involvement,Diversity,Employee satisfaction"
