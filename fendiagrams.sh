db999528a5c21aba51657552f01b2871d7d16ba3
Simon Deeley <simon@remotify.work>
2020-05-26 16:24:18 +0100

    As used in FEN presentation.

--- fendiagrams.sh
+++ fendiagrams.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 bin/focuson --subset "Career stagnation,Career progression model,Alignment" \
 	    --subset "Career stagnation,Career progression model,Alignment,Repeatable processes,Accountability" \
-	    --subset "Hampered collaboration,Effective Meetings,Realtime collaboration tools,Shared and organised information,Asynchronous working,Effective meetings,Repeatable processes,Outcome quality" \
-	    --subset "Loneliness,Planned spontaneity,Buddy system,Flexible working,Community involvement,Inclusion,Alignment,Diversity,Human connection,Employee satisfaction,Retention,Outcome quality" \
-	    --subset "Loneliness,Planned spontaneity,Buddy system,Community involvement,Inclusion,Human connection,Employee satisfaction,Retention" \
-	    --subset "Loneliness,Buddy system,Flexible working,Community involvement,Alignment,Diversity,Outcome quality" \
+			--subset "Hampered collaboration,Realtime collaboration tools,Shared and organised information,Asynchronous working" \
+			--subset "Realtime collaboration tools,Shared and organised information,Asynchronous working,Repeatable processes,Outcome quality,Scalability,Business continuity" \
+			--subset "Shared and organised information,Realtime collaboration tools,Asynchronous working,Inclusion,Retention,Scalability" \
+			--subset "Hampered collaboration,Inclusion,Retention,Repeatable processes,Outcome quality,Scalability,Business continuity" \
+			--subset "Loneliness,Planned spontaneity,Community involvement" \
+			--subset "Planned spontaneity,Community involvement,Inclusion,Human connection,Employee satisfaction,Retention" \
+			--subset "Loneliness,Radical flexibility,Community involvement" \
+			--subset "Loneliness,Radical flexibility,Planned spontaneity,Community involvement,Inclusion,Human connection,Employee satisfaction,Retention,Diversity,Outcome quality" \
+			--subset "Loneliness,Inclusion,Human connection,Employee satisfaction,Retention,Diversity,Outcome quality" \
 	    --subset "Career stagnation,Isolation,Onboarding,Loneliness,Planned spontaneity" \
-	    --subset "Asynchronous working,Flexible working,Community involvement,Diversity,Employee satisfaction"
+	    --subset "Asynchronous working,Radical flexibility,Community involvement,Diversity,Employee satisfaction" \
+			--subset "Results focus,Self-organisation,Asynchronous working,Trust & autonomy,Radical flexibility" \
+			--subset "Supervision,Disengagement,Career stagnation,Place and time differences,Loneliness,Hampered collaboration,Isolation,Physical health,Onboarding,Training juniors" \
+			--subset "Trust & autonomy,Results focus,Career progression model,Self-organisation,Asynchronous working,Radical flexibility,Team agreement,Visible teamwork,Realtime collaboration tools,Effective meetings,Remote working allowance,Planned spontaneity,Community involvement,Shared and organised information,Clarity of vision,Overcommunication,Buddy system,Remote skills training,Remote-specialised talent acquisition,Onboarding bot/checklist" \
+			--subset "Adaptability,Accountability,Scalability,Business continuity,Retention,Repeatable processes,Employee satisfaction,Higher throughput,Outcome quality,Human connection,Inclusion,Personal continuity,Alignment,Diversity"
